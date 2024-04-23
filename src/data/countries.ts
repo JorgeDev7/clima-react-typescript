38b49f687c754dbafe433704c3cdb2fa8b738b34
JorgeDev <jorgehumbertocolindres@gmail.com>
2024-04-23 11:02:09 -0600

    Adding Countries

---
+++ src/data/countries.ts
@@ -0,0 +1,10 @@
+export const countries: Country[] = [
+    { code: 'US', name: 'Estados Unidos' },
+    { code: 'MX', name: 'México' },
+    { code: 'AR', name: 'Argentina' },
+    { code: 'CO', name: 'Colombia' },
+    { code: 'CR', name: 'Costa Rica' },
+    { code: 'ES', name: 'España' },
+    { code: 'PE', name: 'Perú' },
+    { code: 'GT', name: 'Guatemala' }
+];
