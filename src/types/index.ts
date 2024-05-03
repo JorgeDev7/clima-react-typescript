e1f5189ea5970c2dec334b8ed68286064a829d34
JorgeDev <jorgehumbertocolindres@gmail.com>
2024-05-03 12:27:35 -0600

    Type to data from the API

--- src/types/index.ts
+++ src/types/index.ts
@@ -7,3 +7,12 @@ type Country = {
     code: string,
     name: string;
 };
+
+type Weather = {
+    name: string,
+    main: {
+        temp: number,
+        temp_max: number,
+        temp_min: number;
+    };
+};
